947de79b0af7b725522815d9ad1bc9f3a10158fa
a442 <alef321@gmail.com>
2015-08-08 11:21:13 -0300

    Added 'fast' option that forces neighrest neighbor. Useful when interpolation is undesirable

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@
 #define MODE_NORM 0
 #define MODE_LIST 1
 #define MODE_CTL 2
+#define MODE_FAST 3
 static int mode;
 
 /* Supported Formats */
@@ -30,8 +31,9 @@ static struct imageformat *formats[] = {
 static void usage(){
 	printf("USAGE: meh [FILE1 [FILE2 [...]]]\n");
 	printf("       meh -list [LISTFILE]      : Treat file as list of images. Defaults to stdin.\n");
-	printf("       meh -ctl                  : Display files as they are received on stdin\n");
+	printf("       meh -ctl                  : Display files as they are received on stdin.\n");
 	printf("       meh -v                    : Print version and exit.\n");
+  printf("       meh -f                    : Use nearest neighbor interpolation (fast).\n");
 	exit(EXIT_FAILURE);
 }
 
@@ -195,7 +197,7 @@ static int doredraw(struct image **i, int idx, int (*dir)(int), int dstates){
 			return 1;
 		}else if(width && height){
 			if((dstates & SCALED) && (state & LOADED) && !(state & SCALED)){
-				backend_prepare(*i, width, height, !!(state & SCALED));
+				backend_prepare(*i, width, height, !!(state & SCALED) | mode == MODE_FAST);
 				(*i)->state &= LOADED | SLOWLOADED | SCALED | SLOWSCALED;
 
 				/* state should be set by backend */
@@ -310,8 +312,13 @@ int main(int argc, char *argv[]){
 	}else if(!strcmp(argv[1], "-v")){
 		printf("meh version 0.3\n");
 		return 0;
-	}else{
-		mode = MODE_NORM;
+	}else if (!strcmp(argv[1], "-f")){
+    mode = MODE_FAST;
+    images = argv + 2;
+    imageslen = argc - 2;
+    imageidx = 0;
+  } else {
+    mode = MODE_NORM;
 		images = &argv[1];
 		imageslen = argc-1;
 		imageidx = 0;
