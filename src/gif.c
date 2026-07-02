e5b34644f148c66211cc4a66a484c7fd706411fa
Andreas Baumann <mail@andreasbaumann.cc>
2026-07-02 09:56:15 +0200

    fixed macro guard for DGifCloseFile to work for 6.x.x and beyond

--- src/gif.c
+++ src/gif.c
@@ -96,11 +96,11 @@ error:
 
 void gif_close(struct image *img){
 	struct gif_t *g = (struct gif_t *)img;
-#if defined(GIFLIB_MAJOR) && defined(GIFLIB_MINOR) && (GIFLIB_MAJOR == 5 && GIFLIB_MINOR >= 1)
+#if defined(GIFLIB_MAJOR) && defined(GIFLIB_MINOR) && (GIFLIB_MAJOR > 5 || (GIFLIB_MAJOR == 5 && GIFLIB_MINOR >= 1))
 	int ret;
 #endif
 
-#if defined(GIFLIB_MAJOR) && defined(GIFLIB_MINOR) && (GIFLIB_MAJOR == 5 && GIFLIB_MINOR >= 1)
+#if defined(GIFLIB_MAJOR) && defined(GIFLIB_MINOR) && (GIFLIB_MAJOR > 5 || (GIFLIB_MAJOR == 5 && GIFLIB_MINOR >= 1))
 	DGifCloseFile(g->gif,&ret);
 	if(ret != GIF_OK) {
 #if defined(GIFLIB_MAJOR) && GIFLIB_MAJOR >= 5
